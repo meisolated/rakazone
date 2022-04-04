38a16c6698e54d9c5020eae74b5388fa221215cb
Isolated <isolatedbot@gmail.com>
2022-04-05 01:04:58 +0530

    test

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,54 +1,56 @@
 import css from "./Header.module.css"
 import Image from "next/image"
-
+import LoadingBar from '../LoadingBar'
 
 function Header() {
     return (
-        <div className={css.container}>
-            <div className="container-default">
-                <div className={css.header_wrapper}>
-                    <div className={css.left}>
-                        <Image src="https://raka.zone/assets/img/logo.png" className={css.logoimg} alt="a" width={164} height={60} />
-                    </div>
-                    <div className={css.right}>
-                        <nav className={css.nav_menu}>
-                            <ul className={css.header_navigation}>
-                                <li className={css.nav_item_wrapper}>
-                                    <a>Home</a>
-                                </li>
-                                <li className={css.nav_item_wrapper}>
-                                    <a>About</a>
-                                </li>
-                                <li className={css.nav_item_wrapper}>
-                                    <a>Blog</a>
-                                </li>
-                                <li className={css.nav_item_wrapper}>
-                                    <a>Products</a>
-                                </li>
-                            </ul>
-                        </nav>
+        <LoadingBar>
+            <div className={css.container}>
+                <div className="container-default">
+                    <div className={css.header_wrapper}>
+                        <div className={css.left}>
+                            <Image src="https://raka.zone/assets/img/logo.png" className={css.logoimg} alt="a" width={164} height={60} />
+                        </div>
+                        <div className={css.right}>
+                            <nav className={css.nav_menu}>
+                                <ul className={css.header_navigation}>
+                                    <li className={css.nav_item_wrapper}>
+                                        <a>Home</a>
+                                    </li>
+                                    <li className={css.nav_item_wrapper}>
+                                        <a>About</a>
+                                    </li>
+                                    <li className={css.nav_item_wrapper}>
+                                        <a>Blog</a>
+                                    </li>
+                                    <li className={css.nav_item_wrapper}>
+                                        <a>Products</a>
+                                    </li>
+                                </ul>
+                            </nav>
 
-                        <div className={css.menu_button}>
+                            <div className={css.menu_button}>
 
-                            <div className={css.header_menu_button_icon_wrapper}>
-                                <div className={css.icon_wrapper}>
-                                    <div
-                                        className={css.header_menu_button_icon_top}
+                                <div className={css.header_menu_button_icon_wrapper}>
+                                    <div className={css.icon_wrapper}>
+                                        <div
+                                            className={css.header_menu_button_icon_top}
 
-                                    ></div>
-                                    <div className={css.header_menu_button_icon_middle} ></div>
-                                    <div
-                                        className={css.header_menu_button_icon_bottom}
+                                        ></div>
+                                        <div className={css.header_menu_button_icon_middle} ></div>
+                                        <div
+                                            className={css.header_menu_button_icon_bottom}
 
-                                    ></div>
+                                        ></div>
+                                    </div>
                                 </div>
-                            </div>
 
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </LoadingBar>
     )
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import '../styles/globals.css'
 import LoadingBar from '../components/LoadingBar/index.js'
 
 function MyApp({ Component, pageProps }) {
-  return (<><LoadingBar /><Layout><Component {...pageProps} /></Layout></>)
+  return (<Layout><Component {...pageProps} /></Layout>)
 }
 
 export default MyApp
