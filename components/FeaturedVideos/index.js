777aad4f189aea2b6e9dec059bb8c0cb26ba5ffd
Isolated <isolatedbot@gmail.com>
2022-04-05 00:58:16 +0530

    some commmit

--- components/FeaturedVideos/index.js
+++ components/FeaturedVideos/index.js
@@ -16,7 +16,6 @@ function FeaturedVideo() {
                         <Image className={css.video_featured_channel_image} src="https://raka.zone/assets/img/instadp.jpeg" width={94} height={94} loading="eager" alt="Video Featured Channel Image" />
                     </div>
                     <div>
-
                         <h2 className={css.title}>iOS 15 is now released: Top 5 new features!</h2>
                         <div className={css.video_featured_about}>
                             <div className={css.video_featured_duration}>
@@ -29,16 +28,48 @@ function FeaturedVideo() {
                     </div>
                 </div>
             </div>
-            <div className={css.video_featured_secondary_grid}>
-                <div className={css.image_wrapper}>
-                    <Image className={css.video_item_image} src="https://i.ytimg.com/vi/rwgb3sTQ-nc/maxresdefault.jpg" width={540} height={300} alt="" />
-                    <div className={css.video_featured_filter}></div>
-                    <VideoButton w={30} h={30} ww={85} hh={85} />
+            <div className={`${css.video_featured_secondary_grid} pd-bottom `}>
+                <div className={css.video_featured_secondary_wrapper}>
+                    <div className={css.image_wrapper}>
+                        <Image className={css.video_item_image} src="https://i.ytimg.com/vi/rwgb3sTQ-nc/maxresdefault.jpg" width={540} height={300} alt="" />
+                        <div className={css.video_featured_filter}></div>
+                        <VideoButton w={30} h={30} ww={85} hh={85} />
+                    </div>
+                    <div className={css.video_featured_secondary_content}>
+
+                        <div>
+                            <h3 className={css.title}>iOS 15 is now released: Top 5 new features!</h3>
+                            <div className={css.video_featured_about}>
+                                <div className={css.video_featured_duration}>
+                                    <div>12</div>
+                                    <div>&nbsp;min</div>
+                                </div>
+                                <div className={css.video_feature_about_divider}></div>
+                                <div>15/9/21</div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className={css.image_wrapper}>
-                    <Image className={css.video_item_image} src="https://i.ytimg.com/vi/rwgb3sTQ-nc/maxresdefault.jpg" width={540} height={300} alt="" />
-                    <div className={css.video_featured_filter}></div>
-                    <VideoButton w={30} h={30} ww={85} hh={85} />
+                <div className={css.video_featured_secondary_wrapper}>
+                    <div className={css.image_wrapper}>
+                        <Image className={css.video_item_image} src="https://i.ytimg.com/vi/rwgb3sTQ-nc/maxresdefault.jpg" width={540} height={300} alt="" />
+                        <div className={css.video_featured_filter}></div>
+                        <VideoButton w={30} h={30} ww={85} hh={85} />
+                    </div>
+                    <div className={css.video_featured_secondary_content}>
+
+                        <div>
+                            <h3 className={css.title}>iOS 15 is now released: Top 5 new features!</h3>
+                            <div className={css.video_featured_about}>
+                                <div className={css.video_featured_duration}>
+                                    <div>12</div>
+                                    <div>&nbsp;min</div>
+                                </div>
+                                <div className={css.video_feature_about_divider}></div>
+                                <div>15/9/21</div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
