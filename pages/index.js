811650fc73439c2c26cec87f61319c6f812de655
Isolated <isolatedbot@gmail.com>
2022-05-18 04:15:59 +0530

    working on google login

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,31 @@
-import css from "./index.module.css"
-import AboutChannel from "../components/AboutChannel/index.js"
-import SubscribeToMyChannel from "../components/SubscribeToMyChannel/index.js"
-import BuyMerch from "../components/BuyMerch/index.js"
-import axios from "axios"
-import Image from "next/image"
-import { Primary } from "../components/Buttons"
-import VideoItem from "../components/VideoItem"
-import { VideoBtnBig } from "../components/Buttons"
 import { convertToInternationalCurrencySystem } from "../util/functions.js"
+import SubscribeToMyChannel from "../components/SubscribeToMyChannel"
+import BuyMerch from "../components/BuyMerch/index.js"
+import AboutChannel from "../components/AboutChannel"
+import { VideoBtnBig } from "../components/Buttons"
+import VideoItem from "../components/VideoItem"
+import { Primary } from "../components/Buttons"
+import css from "./index.module.css"
+import Image from "next/image"
+import Link from "next/link"
 import moment from "moment"
+import axios from "axios"
+import _ from "lodash"
 
 function Home(props) {
-    let userdata = props.serverdata.userdata[0]
-
-    let liveData = props.serverdata.livedata[0]
-    let mostLiked = props.serverdata.somevideos.mostLiked[0]
-    let mostViewed = props.serverdata.somevideos.mostViewed[0]
-    let mostCommented = props.serverdata.somevideos.mostLiked[0]
-    let recent = props.serverdata.somevideos.recent[0]
+    let streamerData = props.streamerData
+    let liveData = props.liveData
+    let mostLiked = props.sortedVideos.mostLikedVideo[_.random(0, props.sortedVideos.mostLikedVideo.length - 1)]
+    let mostViewed = props.sortedVideos.mostViewedVideo[_.random(0, props.sortedVideos.mostViewedVideo.length - 1)]
+    let mostRecent = props.sortedVideos.mostRecentVideo[_.random(0, props.sortedVideos.mostRecentVideo.length - 1)]
+    let mostRecentFunny = props.sortedVideos.mostRecentFunny[_.random(0, props.sortedVideos.mostRecentFunny.length - 1)]
+    let mostCommented = props.sortedVideos.mostCommentedVideo[_.random(0, props.sortedVideos.mostCommentedVideo.length - 1)]
 
     mostLiked.title = mostLiked.title.length > 60 ? mostLiked.title.substring(0, 60) + "..." : mostLiked.title
     mostViewed.title = mostViewed.title.length > 60 ? mostViewed.title.substring(0, 60) + "..." : mostViewed.title
     mostCommented.title = mostCommented.title.length > 60 ? mostCommented.title.substring(0, 60) + "..." : mostCommented.title
 
-    let whatToShow = liveData.status === "offline" ? recent : liveData
+    let whatToShow = liveData.status === "offline" ? mostRecent : liveData
     let ago = moment(whatToShow.publishedAt * 1000).fromNow()
 
     let youtube_thumnail = `https://i.ytimg.com/vi/${whatToShow.videoId}/maxresdefault.jpg`
@@ -32,6 +34,10 @@ function Home(props) {
     return (
         <div className={css.main}>
             <div className="container-default">
+                <Link href="/api/google">
+                    <a>Login</a>
+                </Link>
+
                 {/* –––––––––––––––––––––––––– HOME HERO –––––––––––––––––––––––––– */}
                 <div className={css.home_hero}>
                     <div className={css.home_hero_left}>
@@ -41,16 +47,14 @@ function Home(props) {
                         <div className="home_hero_channel_about">
                             <div className={css.channel_about_content_top}>
                                 <h1 className={css.title}>RakaZone</h1>
-                                <div className={css.subscriber_count}>{`${userdata.yt_subscribers_count} SUBSCRIBERS`}</div>
+                                <div className={css.subscriber_count}>{`${streamerData.yt_subscribers_count} SUBSCRIBERS`}</div>
                             </div>
                             <p className={css.about}>
-
                                 <span className={css.bold}>Rishab Karanwal</span> most of you know me as Raka, I am a vairty streamer. If you say so.{" "}
                             </p>
                         </div>
                     </div>
                     <p className={css.mobile_about}>
-
                         <span className={css.bold}>Rishab Karanwal</span> most of you know me as Raka, I am a vairty streamer. If you say so.{" "}
                     </p>
                     <div className={css.home_hero_right}>
@@ -127,11 +131,16 @@ function Home(props) {
     )
 }
 
-export async function getServerSideProps(context) {
-    let { data } = await axios.get("http://10.69.69.201:3000/api/v1/all")
+export async function getServerSideProps({ req, res }) {
+    let sortedVideos = await axios.get("http://10.69.69.201:3000/api/v1/sortedVideos").then((res) => res.data)
+    let streamerData = await axios.get("http://10.69.69.201:3000/api/v1/streamerData").then((res) => res.data)
+    let liveData = await axios.get("http://10.69.69.201:3000/api/v1/liveData").then((res) => res.data)
 
-    if (data.message === "success") {
-        return { props: { serverdata: data.data } }
+    if (sortedVideos.message === "success" && streamerData.message === "success" && liveData.message === "success") {
+        sortedVideos = sortedVideos.data.sortedVideos
+        streamerData = streamerData.data.streamerData
+        liveData = liveData.data.liveData
+        return { props: { sortedVideos, streamerData, liveData } }
     } else return { props: {} }
 }
 
