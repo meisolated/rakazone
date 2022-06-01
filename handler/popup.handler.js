823430ab14d395ccad0aec6af59d79e9e3fcaaae
meisolated <isolatedbot@gmail.com>
2022-06-01 11:53:33 +0530

    fixed few bugs

--- handler/popup.handler.js
+++ handler/popup.handler.js
@@ -1,20 +1,34 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import react from "react"
 import { fetcher } from "../util/functions.js"
 import Loading from "../components/Loading"
-export default function () {
-    let [loading, setLoading] = useState(true)
-    const { data, error } = useSWR('api/v1/popups', fetcher)
-    if (error) {
-        console.log(error)
-    }
-    if (data) {
-        setLoading(false)
-    }
-    return (
-        <>
-            {loading ? <Loading /> : <></>}
+import useSWR from "swr"
 
-        </>
-    )
 
+
+export default class popupHandler extends react.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            show: false,
+            first: true,
+            onClose: () => {
+                this.setState({ show: !this.state.show })
+            },
+            fetcher: (...args) => fetcher(...args).then((res) => res.json())
+        }
+    }
+
+    componentDidMount() {
+        let first_pop = localStorage.getItem("pop_status")
+        if (first_pop) {
+            this.setState({ first: false })
+        }
+    }
+
+    render() {
+        return (
+            <></>
+        )
+    }
 }
--- pages/Test/index.js
+++ pages/Test/index.js
@@ -6,31 +6,26 @@ import { useState, useEffect, createContext, useContext } from "react"
 import css from "./Test.module.css"
 import Head from "next/head"
 
-const Context = createContext()
 
 export default function Test(props) {
+    useEffect(() => {
+        let first_pop = localStorage.getItem("pop_status")
+        if (first_pop) {
+            setFirst(false)
+        }
+    }, [])
     const [show, setShow] = useState(false)
     const [first, setFirst] = useState(true)
     const onClose = () => {
         setShow(!show)
     }
-
-    useEffect(() => {
-        let first_pop = localStorage.getItem("pop_status")
-        console.log(first_pop == 1)
-        if (first_pop) {
-            setFirst(false)
-        }
-    }, [])
-
-
     return (
         <>
             <Head>
                 <title>Test - RakaZone</title>
             </Head>
             <div className="container-default">
-                <a onClick={() => setShow(true)}>{first ? "Show Popup" : "Not first"}</a>
+                <a onClick={() => (first ? setShow(true) : [])}>{first ? "Show Popup" : "Not first"}</a>
                 {show ? <Popups onClose={onClose} /> : <></>}
             </div>
         </>
