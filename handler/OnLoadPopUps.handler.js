5a6886cddb975b6fab07326b2186b31bc929e1f8
Isolated <isolatedbot@gmail.com>
2022-06-07 08:29:35 +0530

    working on bottom navbar

--- handler/OnLoadPopUps.handler.js
+++ handler/OnLoadPopUps.handler.js
@@ -1,22 +1,17 @@
-import { Subject } from 'rxjs'
-import { filter } from 'rxjs/operators'
-
+import { Subject } from "rxjs"
+import { filter } from "rxjs/operators"
 
 export const toastService = {
     onToast,
-    success,
-    error,
-    info,
-    warn,
-    toast,
-    clear
+    show,
+    clear,
 }
 
 export const ToastType = {
-    Success: 'Success',
-    Error: 'Error',
-    Info: 'Info',
-    Warning: 'Warning'
+    Success: "Success",
+    Error: "Error",
+    Info: "Info",
+    Warning: "Warning",
 }
 
 const toastSubject = new Subject()
@@ -24,30 +19,18 @@ const defaultId = "default-toast"
 
 // enable subscribing to toasts observable
 function onToast(id = defaultId) {
-    return toastSubject.asObservable().pipe(filter(x => x && x.id === id))
+    return toastSubject.asObservable().pipe(filter((x) => x && x.id === id))
 }
 
 // convenience methods
-function success(message, options) {
-    toast({ ...options, type: ToastType.Success, message })
-}
-
-function error(message, options) {
-    toast({ ...options, type: ToastType.Error, message })
-}
-
-function info(message, options) {
-    toast({ ...options, type: ToastType.Info, message })
-}
-
-function warn(message, options) {
+function show(message, options) {
     toast({ ...options, type: ToastType.Warning, message })
 }
 
 // core toast method
 function toast(toast) {
     toast.id = toast.id || defaultId
-    toast.autoClose = (toast.autoClose === undefined ? true : toast.autoClose)
+    toast.autoClose = toast.autoClose === undefined ? true : toast.autoClose
     toastSubject.next(toast)
 }
 
--- pages/Test/index.js
+++ pages/Test/index.js
@@ -5,28 +5,56 @@ import { useState, useEffect, createContext, useContext } from "react"
 import css from "./Test.module.css"
 import Head from "next/head"
 import Modal from "../../components/Modal/index.js"
-
+import homeIcon from "../../assets/svg/home.svg"
+import homeIcon_clicked from "../../assets/svg/clicked_home.svg"
+import Image from "next/image"
 
 export default function Test(props) {
+    let [clicked, setClicked] = useState(false)
+    let [img, setImg] = useState(homeIcon)
+
+    useEffect(() => {
+        setImg(clicked ? homeIcon_clicked : homeIcon)
+    }, [clicked])
     // useEffect(() => {
     //     let first_pop = localStorage.getItem("pop_status")
     //     if (first_pop) {
     //         setFirst(false)
     //     }
     // }, [])
-    const [show, setShow] = useState(false)
-    const [first, setFirst] = useState(true)
-    const onClose = () => {
-        setShow(!show)
-    }
+    // const [show, setShow] = useState(false)
+    // const [first, setFirst] = useState(true)
+    // const onClose = () => {
+    //     setShow(!show)
+    // }
     return (
         <>
             <Head>
                 <title>Test - RakaZone</title>
             </Head>
-            <div className="container-default">
+            {/* <div className="container-default">
                 <a onClick={() => (first ? setShow(true) : [])}>{first ? "Show Popup" : "Not first"}</a>
                 {show ? <Modal onClose={onClose} /> : <></>}
+            </div> */}
+            <div className={css.bottom_navbar_container}>
+                <div className={css.bottom_navbar}>
+                    <div className={`${css.home_button} ${css.nav_buttom} ${clicked ? css.clicked : []}`} onClick={() => setClicked(true)}>
+                        <Image src={img} alt="" />
+                        <a>Home</a>
+                    </div>
+                    <div className={`${css.shop_button} ${css.nav_buttom}`}>
+                        <Image src={homeIcon} alt="" />
+                        <a>Home</a>
+                    </div>
+                    <div className={`${css.community_button} ${css.nav_buttom}`}>
+                        <Image src={homeIcon} alt="" />
+                        <a>Home</a>
+                    </div>
+                    <div className={`${css.profile_button} ${css.nav_buttom}`}>
+                        <Image src={homeIcon} alt="" />
+                        <a>Home</a>
+                    </div>
+                </div>
             </div>
         </>
     )
