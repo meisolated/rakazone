962cfe70812115d48bfa0ca594e306f0d4673b7b
meisolated <isolatedbot@gmail.com>
2022-08-18 06:19:33 +0530

    fixed few bugs

--- src/pages/index.js
+++ src/pages/index.js
@@ -261,8 +261,7 @@ export async function getServerSideProps({ req, res }) {
  const ip = forwarded ? forwarded.split(/, /)[0] : req.connection.remoteAddress
  console.log(ip)
  let sortedVideos = await axios.get(`${publicRuntimeConfig.apiUrl}content`, { withCredentials: true }).then((res) => res.data)
- let streamerData = await axios.get(`${publicRuntimeConfig.apiUrl}streamerData`, { withCredentials: true }).then((res) => res.data)
-
+ let streamerData = await axios.get(`${publicRuntimeConfig.apiUrl}streamerdata`, { withCredentials: true }).then((res) => res.data)
  if (sortedVideos.message === "success" && streamerData.message === "success") {
   sortedVideos = sortedVideos.data.sortedVideos
   streamerData = streamerData.data.streamerData
